b199a6c98d2fe478742a95a44fd4e4dc2312c6e4
Hannes Probst <hannes@moped.com>
2012-11-03 19:51:19 +0100

    create user on oauth ready

--- bringmehome/bringmehome/apps/oauth/views.py
+++ bringmehome/bringmehome/apps/oauth/views.py
@@ -12,7 +12,8 @@ from django.http import HttpResponse
 
 from bringmehome import settings
 
-from bringmehome.rest.models import UserProfile
+from bringmehome.apps.rest.models import UserProfile
+
 import foursquare
 def callback(request):
 	code = request.REQUEST.get('code')
@@ -28,6 +29,8 @@ def callback(request):
 	# Get the user's data
 	fsq_user = client.users()
 
-	u = UserProfile.objects.get_or_create(foursquare_id=fsq_user.id, oauth_token=access_token)
+	u, created = UserProfile.objects.get_or_create(foursquare_id=fsq_user.get('user').get('id'), oauth_token=access_token)
 	u.save()
+	print "created user ", u
+	print "created new user? ", created
 	return redirect('home', {'user_id': u.uuid})
