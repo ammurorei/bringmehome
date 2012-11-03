d3a57256e5338c1de3709b30c7f23a00041c07f5
Hannes Probst <hannes@moped.com>
2012-11-03 15:00:08 +0100

    all the files

--- bringmehome/bringmehome/settings.py
+++ bringmehome/bringmehome/settings.py
@@ -37,13 +37,13 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = here('media/')
+MEDIA_ROOT = here('/media/')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
 MEDIA_URL = '/media/'
-
+ADMIN_MEDIA_PREFIX = '/media/'
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
