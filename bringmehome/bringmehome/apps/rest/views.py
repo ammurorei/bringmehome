bfd16ecc1ca3d401746c5d0159bd76e446b2f29d
Hannes Probst <hannes@moped.com>
2012-11-03 20:26:10 +0100

    oauth ready

--- bringmehome/bringmehome/apps/rest/views.py
+++ bringmehome/bringmehome/apps/rest/views.py
@@ -137,3 +137,7 @@ def query_way_home(request, user_id):
 			#print "====================================================="
 			#print "====================================================="
 	return return_data
+
+
+#def handle_push(request):
+
