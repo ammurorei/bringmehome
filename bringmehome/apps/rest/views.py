f5be2c60e792b57f0207b46b21ee7369de0f1897
Isac <isac.petruzzi@zemanta.com>
2012-11-05 03:05:17 +0100

    translate geolocation data to BVGs stupid implementation thereof

--- bringmehome/apps/rest/views.py
+++ bringmehome/apps/rest/views.py
@@ -18,6 +18,7 @@ from django.http import HttpResponse, Http404
 BVG_ROOT = 'http://mobil.bvg.de'
 BVG_URL = 'http://mobil.bvg.de/Fahrinfo/bin/query.bin/dox'
 BVG_MATCH = re.compile('^(conL)|(conD)$')
+GEO_MATCH = re.compile('([0-9]{2})\.([0-9]{6}).*') #from BVG website
 
 EXCLUDE = {
 	"English" : 0,
@@ -113,11 +114,16 @@ def query_way_home(request):
 	now = datetime.datetime.now() #locale should be set to berlin
 	journey_time = now + datetime.timedelta(minutes=60)
 
+	lng = str(user_data['venue']['location']['lng'])
+	lat = str(user_data['venue']['location']['lat'])
+
+	lng_match = re.match(GEO_MATCH, lng)
+	lat_match = re.match(GEO_MATCH, lat)
+
 	data = urllib.urlencode({
 			'REQ0HafasInitialSelection':0,
 			'queryDisplayed': True,
-			#'SID':'A=16@X=%s@Y=%s@O=Von hier starten' % (user_data['venue']['location']['lng'], user_data['venue']['location']['lat']), #Checkin location data
-			'SID':'A=16@X=13411086@Y=52551357@O=Von hier starten',
+			'SID':'A=16@X=%s%s@Y=%s%s@O=Von hier starten' % (lng_match.group(1), lng_match.group(2), lat_match.group(1), lat_match.group(2)), #Checkin location data translated to BVG specs
 			'REQ0JourneyStopsZ0A': 255,
 			'REQ0JourneyStopsZ0G': user.address.encode('utf-8'),
 			#REQ0JourneyStopsZ0ID: 0,
